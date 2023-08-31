f7b65abc28c3b2b777ec4aa0c6610ad7a62eddf3
Jesus Biaggioni <jesu772@gmail.com>
2023-08-31 14:45:27 -0300

    Removed some leftovers code

--- coffeeshop-domain/src/main/java/com/trafilea/coffeeshopdomain/model/Cart.java
+++ coffeeshop-domain/src/main/java/com/trafilea/coffeeshopdomain/model/Cart.java
@@ -3,13 +3,4 @@ package com.trafilea.coffeeshopdomain.model;
 import java.util.List;
 
 public record Cart(String cartId, Long userId, List<Product> products) {
-
-    public Cart withId(String cartId) {
-        return new Cart(cartId, userId, products);
-    }
-
-    public static Cart newCart(Long userId) {
-        return new Cart(null, userId, List.of());
-    }
-
 }
